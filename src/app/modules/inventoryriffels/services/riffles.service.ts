c21225badb415323b6d921661f865d8c7dde468a
shenouda-saleh <eng.shenouda.saleh@gmail.com>
2022-10-24 13:38:44 +0200

    edit riffle

--- src/app/modules/inventoryriffels/services/riffles.service.ts
+++ src/app/modules/inventoryriffels/services/riffles.service.ts
@@ -5,6 +5,7 @@ import { HttpReponseModel } from 'src/app/core-module/models/ResponseHttp';
 import { LookUpModel } from 'src/app/shared-module/models/lookup';
 import { HttpPaths } from '../../auth/Enums/HttpPaths.enum';
 import { IAddRiffles } from '../models/IAddRiffles.interface';
+import { IRiffle } from '../models/IRiffle.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,19 +13,24 @@ import { IAddRiffles } from '../models/IAddRiffles.interface';
 export class RifflesService {
   bSubject = new BehaviorSubject<boolean>(false);
 
-constructor(private http:CommonHttpService) { }
+  constructor(private http: CommonHttpService) { }
 
 
 
-getCommmittee() {
-  return this.http.CommonGetRequests(`${localStorage.getItem('companyLink')}${HttpPaths.API_LIST_OF_COMMMITTEE}`).pipe(
-    map(Items => Items.data.map((Item: any) => ({ Id: Item.id, Name: Item.name }) as LookUpModel))
-  );
-}
+  getCommmittee() {
+    return this.http.CommonGetRequests(`${localStorage.getItem('companyLink')}${HttpPaths.API_LIST_OF_COMMMITTEE}`).pipe(
+      map(Items => Items.data.map((Item: any) => ({ Id: Item.id, Name: Item.name }) as LookUpModel))
+    );
+  }
 
+  getRiffleById(riffleId: number): Observable<IRiffle> {
+    return this.http.CommonGetRequests(`${localStorage.getItem('companyLink')}${HttpPaths.API_ADD_COUNTING_PROCESS}`).pipe(
+      map(items => items.data as IRiffle)
+    )
+  }
 
-addRiffle(model: IAddRiffles): Observable<HttpReponseModel> {
-  return this.http.CommonPostRequests(model, `${localStorage.getItem('companyLink')}${HttpPaths.API_ADD_COUNTING_PROCESS}`)
-}
+  addRiffle(model: IAddRiffles): Observable<HttpReponseModel> {
+    return this.http.CommonPostRequests(model, `${localStorage.getItem('companyLink')}${HttpPaths.API_ADD_COUNTING_PROCESS}`)
+  }
 
 }
